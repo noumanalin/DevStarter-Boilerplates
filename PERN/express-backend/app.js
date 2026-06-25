24b885c659ee6bd27fe737abc845ebe2e4b780bc
naumanalin <naumanalin865@gmail.com>
2026-06-25 21:25:45 +0500

    update backend cors

--- PERN/express-backend/app.js
+++ PERN/express-backend/app.js
@@ -24,7 +24,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:3000",
-
+    "https://nadashboard-boilerplates.vercel.app",
+    "https://noumanali.vercel.app",
     ],
   credentials: true
 }));
